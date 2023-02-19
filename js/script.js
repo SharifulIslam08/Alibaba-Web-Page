56974af2ad8e370065b5137392c4727fadb554b2
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-02-19 19:04:50 +0600

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var sellHover = document.getElementById("top_navber");
+sellHover.addEventListener('mouseover',function(){
+    var a= document.getElementById("top_navber_sell_hover");
+    console.log(a.style.display);
+    // if(a.style.display!="none")
+    // {
+    //     a.style.display="none";
+    // }
+    // else{
+    //     a.style.display="block";
+    // }
+})
